09840f04f8ce2d5060c2f75c5cb129768665d12a
Olya Yakymovych <msolya@outlook.com>
2017-12-04 22:02:24 +0200

    inital commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.add-class').on('click', function() {
+	$('.add-class').addClass('test-class');
+});
+
+$('.remove-class').on('click', function() {
+	$('.remove-class').removeClass('btn-lg');
+});
